aef86621c3cc577fd839ae1923b8d45f1cea0308
localadmin <localadmin@CLA-N-STB08-01.BoilerAD.Purdue.edu>
2019-11-08 12:52:12 -0500

    Improvement to calling the R files

--- GUImorph/startGUI.R
+++ GUImorph/startGUI.R
@@ -1,38 +1,35 @@
 rm(list=ls())
 
 library(GUImorph)
-GUImorph()
+library(testthat)
 
-# test functions
 require(tcltk)
 require(tcltk2)
 require(parallel)
 require(geomorph)
 require(Rvcg)
+require(testthat)
 require(Morpho)
-source("R/rtkogl.r")
-source("R/curve.r")
-source("R/3dDigitize.digitize.r")
-source("R/3dDigitize.geomorph.r")
-source("R/GUImorph.r")
-source("R/3dDigitize.surface.r")
-source("R/geomorph.support.code.r")
-
 source("R/rtkogl.r")
 source("R/3dDigitize.curve.r")
 source("R/3dDigitize.digitize.r")
 source("R/3dDigitize.geomorph.r")
+source("R/3dDigitize.main.r")
+source("R/geomorph.support.code.r")
 source("R/GUImorph.r")
 source("R/3dDigitize.surface.r")
 source("R/geomorph.support.code.r")
+
+GUImorph()
+
 #digitize3D <-function() {
     e <- new.env()
     class(e) <- "main"
     ui(e)
     init(e)
 #}
-library("roxygen2") 
-library("devtools")
+#library("roxygen2") 
+#library("devtools")
 #lines<-readLines("DESCRIPTION")
 #lines[4]<-paste("Version: 1.0.2.",format(Sys.time(), "%m.%d.%Y.%H.%M"),sep="")
 #lines[5]<-paste("Date: ",Sys.Date(),sep="")
