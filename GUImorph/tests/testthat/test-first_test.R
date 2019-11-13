a8f1e7d5e48d081469869c737cc85c515b01b674
localadmin <localadmin@CLA-N-STB08-01.BoilerAD.Purdue.edu>
2019-11-13 12:43:47 -0500

    Got UI launching to be callable by test case, TODO: make this actally done by passing in command line arguments instead?

--- GUImorph/tests/testthat/test-first_test.R
+++ GUImorph/tests/testthat/test-first_test.R
@@ -1,21 +1,6 @@
-library(GUImorph)
-library(devtools)
-library(testthat)
-
-install.packages("tcltk")
-install.packages("tcltk2")
-install.packages("parallel")
-install.packages("gemorph")
-install.packages("GUImorph")
-install.packages("codetools")
-install.packages("vegan")
-install.packages("Rvcg")
-install.packages("testthat")
-install.packages("Morpho")
-install.packages("usethis")
-
 test_that("multiplication works", {
-    source("startGUI.R")
+    list.files(getwd())
+    StartFunction(5)
     
     expect_equal(2 * 2, 4)
     #result <- StartFunction(1)
